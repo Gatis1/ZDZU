0a0d252ab11ed40e217b3ffc805821a5c9b7695a
lboy22 <44554960+lboy22@users.noreply.github.com>
2024-04-22 12:54:47 -0700

    added health system to enemy and damage (given/received) values.

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -6,10 +6,13 @@ public class Enemy : MonoBehaviour
 {
     public Player player;
     [SerializeField] private float mvSpd = 1.0f;
+    [SerializeField] float healthValue, attackValue;
+
     // Start is called before the first frame update
     void Start()
     {
         player = GameObject.FindObjectOfType(typeof(Player)) as Player;
+        CheckHealth();
     }
 
     // Update is called once per frame
@@ -19,4 +22,65 @@ public class Enemy : MonoBehaviour
     }
 
     private void swarm(){transform.position = Vector3.MoveTowards(transform.position, player.transform.position, mvSpd * Time.deltaTime);}
+
+    public void UpdateHealth()
+    {
+        switch (this.gameObject.name)
+        {
+            case "computer science":
+                healthValue = 3.0f;
+                break;
+            case "business":
+                healthValue = 5.0f;
+                break;
+            case "art":
+                healthValue = 10.0f;
+                break;
+            case "music theory":
+                healthValue = 7.0f;
+                break;
+        }
+    }
+
+    public bool CheckHealth()
+    {
+        bool healthState = false;
+        if(healthValue <= 0.0f)
+        {
+            healthState = false;
+        }
+        return healthState;
+    }
+
+    private float CheckAttack(Collision collision)
+    {
+        switch(collision.gameObject.tag)
+        {
+            /*
+             * Speed values for each projectile would be good, this gives an advantage and disadvantage to each weapon/ball.
+             */
+            case "baseball":
+                attackValue = 1.0f;
+                break;
+            case "volleyball":
+                attackValue = 3.0f;
+                break;
+            case "football":
+                attackValue = 6.0f;
+                break;
+        }
+        return attackValue;
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if(collision.gameObject.tag == "ball")
+        {
+            CheckAttack(collision);
+        }
+        if(!CheckHealth())
+        {
+            Destroy(this.gameObject);
+        }
+    }
 }
