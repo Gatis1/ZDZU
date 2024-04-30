ecf6f2c371640f2e3f01c390d879e61f51782000
JonnyTheKing-3 <jonathangrcrdrgz@gmail.com>
2024-04-29 22:28:37 -0700

    Trying different ways to get the ball shooting

--- Assets/Scripts/Shooting.cs
+++ Assets/Scripts/Shooting.cs
@@ -5,17 +5,36 @@ using UnityEngine;
 
 public class Shooting : MonoBehaviour
 {
+    [Header("REFERENCES")] 
+    public GameObject ball;
+    public static bool ReadyToShootBall = false;
+
+    [Header("TARGET")]
+    public Transform target;
+    public float TimeForBallToReachEnemy;
+    
+    [Header("UI AND ANIMATION")]
     public Button shootingButton;
     public Image cursor;
     public LayerMask EnemyLayerMask;
     public Animator animator;
 
-    public bool test = false;
-
+    public float test = 0;
+    
     void Start()
     {
         // Add listener to the button's onClick event
         shootingButton.onClick.AddListener(Shoot);
+        target = null;
+        
+        
+        Debug.Log(Mathf.MoveTowards(test, 10, 3));
+        Debug.Log(Mathf.MoveTowards(test, 10, 3));
+        Debug.Log(Mathf.MoveTowards(test, 10, 3));
+        Debug.Log(Mathf.MoveTowards(test, 10, 3));
+        Debug.Log(Mathf.MoveTowards(test, 10, 3));
+        Debug.Log(Mathf.MoveTowards(test, 10, 3));
+        Debug.Log(Mathf.MoveTowards(test, 10, 3));
     }
     
     public void Shoot()
@@ -29,21 +48,27 @@ public class Shooting : MonoBehaviour
         // if the cursor hits an enemy
         if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, EnemyLayerMask))
         {
-            // Do something
             Debug.Log(hit.transform.gameObject.name + " takes X amount of DAMAGE");
+            // Record the hit's transform
+            target = hit.transform;
         }
     }
 
+    // This method below depends on the animation script for baseball
     private void Update()
     {
-        if (!animator.GetCurrentAnimatorStateInfo(0).IsName("baseball"))
+        // If the animation ends start ball movement
+        if (ReadyToShootBall)
         {
-            // Animation has ended, perform your actions here
-            test = true;
-        }
-        else
-        {
-            test = false;
+            // If we have a target, move the ball towards the enemy
+            ball.transform.position = Vector3.MoveTowards(ball.transform.position, target.position, TimeForBallToReachEnemy);
+            
+            // if the ball has reached the enemy, 
+            if (ball.transform.position == target.position)
+            {
+                target = null;
+                ReadyToShootBall = false;
+            }
         }
     }
 }
--- Assets/Scripts/baseballAnimationEvents.cs
+++ Assets/Scripts/baseballAnimationEvents.cs
@@ -1,38 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
-using System.Diagnostics;
 using UnityEngine;
 
 public class baseballAnimationEvents : StateMachineBehaviour
 {
-    public int counter = 0; 
-    // OnStateEnter is called when a transition starts and the state machine starts to evaluate this state
-    //override public void OnStateEnter(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
-
-    // OnStateUpdate is called on each Update frame between OnStateEnter and OnStateExit callbacks
-    //override public void OnStateUpdate(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    
-    //}
+    public int counter = 0;
 
     // OnStateExit is called when a transition ends and the state machine finishes evaluating this state
     override public void OnStateExit(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
     {
         counter++;
+        // Once the animation ends, we are ready to SHOOT the ball
+        Shooting.ReadyToShootBall = true;
     }
-
-    // OnStateMove is called right after Animator.OnAnimatorMove()
-    //override public void OnStateMove(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that processes and affects root motion
-    //}
-
-    // OnStateIK is called right after Animator.OnAnimatorIK()
-    //override public void OnStateIK(Animator animator, AnimatorStateInfo stateInfo, int layerIndex)
-    //{
-    //    // Implement code that sets up animation IK (inverse kinematics)
-    //}
 }
